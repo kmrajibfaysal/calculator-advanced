851503895ddcbcc9688eb981db1c9eba8de68b59
K. M. Rajib Faysal <kmrajibfaysal@gmail.com>
2022-02-15 12:12:37 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+Just install all the packages with `npm install` and to use tailwind use `npm run build` command. And all the linting and auto formatting are one the go! Cheers!
+use `di` to ignore node_moudules file being ignored in dropbox folder
---
+++ src/index.js
@@ -0,0 +1,12 @@
+// const fib = (n) => {
+//     if (n === 1) {
+//         return [0, 1];
+//     }
+//     const arr = fib(n - 1);
+//     arr.push(arr[arr.length - 1] + arr[arr.length - 2]);
+//     return arr;
+// };
+
+// const a = 5;
+
+// const b = 4;
